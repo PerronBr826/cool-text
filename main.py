80b8d53fe843ba8ab79578a6058342e23fecf8d0
PerronBr826 <PerronBr826@northwested.org>
2025-03-10 11:08:13 -0400

    cool

--- main.py
+++ main.py
@@ -30,6 +30,15 @@ def main():
     while running:
         running = handle_events()
         screen.fill(config.WHITE) # Use color from config
+        
+        draw.draw_text(screen, [30,40], "You lose!", 30, bold=True)
+        draw.draw_text(screen, [50,70], "You win!", 50, italic=True)
+        draw.draw_text(screen, [70,70], "You are annoying!", 10)
+        draw.draw_text(screen, [90,40], "Video Game Text", 50)
+        intensity = 2
+        for i in range(1):
+            draw.draw_text(screen, [40 + random.randrange(-1,1) * intensity, 200 + random.randrange(-1,1) * intensity], "OH NO!", 50, bold=True, italic=True)
+        pygame.display.flip()
 
         # Limit the frame rate to the specified frames per second (FPS)
         clock.tick(config.FPS) # Use the clock to control the frame rate
