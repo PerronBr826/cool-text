e62b571af866a22115c5dcd7975ce8b88aea38c3
PerronBr826 <PerronBr826@northwested.org>
2025-03-12 09:18:49 -0400

    Update draw.py

--- draw.py
+++ draw.py
@@ -14,13 +14,21 @@ def draw_line(screen, shape):
 def draw_square(screen, shape):
     pygame.draw.rect(screen, shape['color'], (shape['position'][0] - shape['radius'], shape['position'][1] - shape['radius'], shape['radius'] * 2, shape['radius'] * 2))
 
-def draw_text(screen, font_pose, text="No text given!", font_size=10, font_name="DejaVuSans.ttf", font_color=(0,0,0), italic=False, bold=False):
+def draw_text(screen, font_pose, text="No text given!", font_size=10, font_name=None, font_color=(0,0,0), italic=False, bold=False, rotation=0):
     pygame.font.init()
-    font = pygame.font.Font(font_name, font_size)
+    if font_name:
+        font = pygame.font.Font(font_name, font_size)
+    else:
+        font = pygame.font.Font(font_name, font_size)
 
     font.set_italic(italic)
     font.set_bold(bold)
 
     text = font.render(text, False, font_color)
+
+    # Rotate Text
+    if rotation != 0:
+        text = pygame.transform.rotate(text, rotation)
+
     screen.blit(text, font_pose)
     
